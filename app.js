92e4c3a260ab61ec4173e49598dd6adf7b3193d7
Izabella Arya <izabellabaer1@Izabella-Baers-MacBook-Pro.local>
2017-04-17 19:32:20 -0700

    added object and got three photos on the page to randomly generate at refresh

--- app.js
+++ app.js
@@ -11,15 +11,56 @@
 // The thing you want to build today will select three random photos from the image directory and display them side-by-side-by-side in the browser window.
 var counter = 0;
 
-var images = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg', 'scissors.jpg', 'shark.jpg', 'sweep.png', 'tauntaun.jpg', 'unicorn.jpg', 'usb.gif', 'wine-glass.jpg', 'water-can.jpg'];
+var allImages = [];
+
+new PageImage ('star wars backpack', 'bag.jpg', 'bag-id', allImages);
+
+new PageImage ('banana slicer', 'banana.jpg', 'banana-id', allImages);
+
+new PageImage ('bathroom tablet holder', 'bathroom.jpg', 'bathroom-id', allImages);
+
+new PageImage ('yellow boots', 'boots.jpg', 'boots-id', allImages);
+
+new PageImage ('breakfast machine', 'breakfast.jpg', 'breakfast-id', allImages);
+
+new PageImage ('meatball bubble gum', 'bubblegum.jpg', 'bubblegum-id', allImages);
+
+new PageImage ('red chair', 'chair.jpg', 'chair-id', allImages);
+
+new PageImage ('green monster', 'cthulhu.jpg', 'cthulhu-id', allImages);
+
+new PageImage ('duck mouthed dog', 'dog-duck.jpg', 'dog-duck-id', allImages);
+
+new PageImage ('canned dragon meat', 'dragon.jpg', 'dragon-id', allImages);
+
+new PageImage ('spoon pen', 'pen.jpg', 'pen-id', allImages);
+
+new PageImage ('pet dusting boots', 'pet-sweep.jpg', 'pet-sweet-id', allImages);
+
+new PageImage ('pizza scissors', 'scissors.jpg', 'scissors-id', allImages);
+
+new PageImage ('shark sleeping bag', 'shark.jpg', 'shark-id', allImages);
+
+new PageImage ('baby dusting onesie', 'sweep.png', 'sweep-id', allImages);
+
+new PageImage ('tauntaun sleeping bag', 'tauntaun.jpg', 'tauntaun-id', allImages);
+
+new PageImage ('canned unicorn meat', 'unicorn.jpg', 'unicorn-id', allImages);
+
+new PageImage ('octopus tentacle usb', 'usb.gif', 'usb-id', allImages);
+
+new PageImage ('water can artwork', 'water-can.jpg', 'water-can-id', allImages);
+
+new PageImage ('wine glass artwork', 'wine-glass.jpg', 'wine-glass-id', allImages);
 
 
-function ImageGame(name){
+
+function PageImage(name, filepath, id, allImages){
   this.name = name;
-  this.filepath = 'img/' + name;
-  // this.timesShown = timesShown;
-  // this.timesClicked = timesClicked;
-  // this.id = 'product-list';
+  this.filepath = 'img/' + filepath;
+  this.timesShown = 0;
+  this.timesClicked = 0;
+  allImages.push(this);
 
 }
 
@@ -30,6 +71,43 @@ function ImageGame(name){
 
 function generateRandomImage(){
   var randomImageSelection = Math.floor(Math.random()* (19 - 0 + 1));
-  images[randomImageSelection];
+  return allImages[randomImageSelection];
   // console.log(images[randomImageSelection]);
 }
+
+function imageAdd(){
+  var newImage;
+  var randomImage;
+  for (var i = 0; i < 3; i++) {
+    randomImage = generateRandomImage();
+    var myImageBox = document.getElementById('images-selection');
+    newImage = document.createElement('img');
+    newImage.setAttribute('src', 'alt');
+    newImage.src = randomImage.filepath;
+    // newImage.alt = randomImage.
+    myImageBox.appendChild(newImage);
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+imageAdd();
+
+// generateRandomImage();
